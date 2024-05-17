0cb4c562da3fc30e122be2bdc8d0d0137dd91c86
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-05-17 19:03:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(0);
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          num={i}
+          title={el.title}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        num={22}
+        title="Is Real Madrid Wining UCL?"
+        key="Is Real Madrid Wining UCL?"
+      >
+        <p>Real Madrid Won The 20 games in La Liga : </p>
+        <ul>
+          <li>Won La Liga Title</li>
+          <li>won agains Barcalona home and away matches</li>
+        </ul>
+      </AccordionItem>
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, key, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => handleToggle(num)}
+      key={key}
+    >
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
